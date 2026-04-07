ab5136a3b065d62123ae333eddb6aab88d74e682
Eben-prog <175055717+Eben-prog@users.noreply.github.com>
2026-04-07 09:04:30 +0300

    First commit

---
+++ developerTools/1.js
@@ -0,0 +1,8 @@
+//print message that tells the user to inter two var and add them
+//input variable that accepts two numbers from the user.
+//Two int variables to store the two numbers 
+//a sum variable that adds the two variables
+//print the result
+
+
+//print the 
