2a5eabfaca5101d47629ca5f82f57d4245b29589
Eben-prog <175055717+Eben-prog@users.noreply.github.com>
2026-04-07 09:18:24 +0300

    Second commit

--- developerTools/1.js
+++ developerTools/1.js
@@ -5,4 +5,26 @@
 //print the result
 
 
-//print the 
+//print message telling the user to inter two numbers
+//int var a stores first input
+//int var b stores second input
+//input var a and var b
+// var sum that adds a + b
+//output sum 
+
+//(BROWSER BASED)
+//alert msg that tells the user to enter two numbers
+//first let holds the first number entered
+//second let holds the second number
+
+alert("Enter two numbers");
+
+let a = prompt("Enter the first number");
+let b = prompt("Enter second number");
+
+a = Number(a);
+b = Number(b);
+
+let sum = a + b;
+
+alert("Sum is: " + sum);
